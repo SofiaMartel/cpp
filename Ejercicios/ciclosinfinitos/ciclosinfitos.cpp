85d5e99308d99c75a34a3397b19a3bc41800b2f1
Sofia Martel <karimartel77@gmail.com>
2022-06-20 15:12:32 -0600

    ciclo infinito

---
+++ Ejercicios/ciclosinfinitos/ciclosinfitos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int ciclos = 0;
+	
+	while(ciclos < 100){
+		
+	cout << "*";
+	
+	ciclos = ciclos +1;	
+	}
+	return 0;
+}
