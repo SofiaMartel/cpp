1c67364d9787a1adfe353d717317d430e50e6116
Sofia Martel <karimartel77@gmail.com>
2022-06-29 14:57:20 -0600

    Suma de pares

---
+++ Ejercicios/Suma de pares/sumadepares.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int pares=0;
+       int total;
+	
+	for(int i=1; i<=10; i++){
+		
+	if(i%2 == 0){
+	pares =pares+i;	
+	}
+	cout<< i <<"";
+	}
+	
+	cout << endl;
+	cout << endl;
+	cout << "total pares:"<<pares<<endl;
+	return 0;
+}
