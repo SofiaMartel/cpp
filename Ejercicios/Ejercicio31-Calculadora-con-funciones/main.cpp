6adb96de5ee5c93811878e485320ef203a0d8c91
Sofia Martel <karimartel77@gmail.com>
2022-07-12 21:12:04 -0600

    Ejercicio 31 Calculadora con Funciones

---
+++ Ejercicios/Ejercicio31-Calculadora-con-funciones/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int sumar(int a, int b)
+{
+    return a+b;
+}
+int calculadora(int a, int b, char operador)
+{
+    if(operador =='+')
+    {
+        return sumar(a,b);
+
+    }
+    return -1;
+}
+int main(int argc, char const *argv[])
+{
+    int n1 = 0;
+    int n2 = 0;
+    char operador;
+
+    cout<<"ingrese a:";
+    cin >> n1;
+    cout<<"ingrese b:";
+    cin >> n2;
+    cout<<"ingrese el operador(+, -, *, /):";
+    cin>>operador;
+
+    cout << "resultado de:"<< n1 << operador <<  n2  << "es:";
+    cout << calculadora(n1, n2, operador);
+
+    return 0;
+}
